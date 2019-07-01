29cdc1775f86cdad39725abe9c38f9d415a3f0cc
Donelle Reed <donellereed@gmail.com>
2019-07-01 23:35:27 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -11,3 +11,16 @@ function nowServing (queue){
     return "There is nobody waiting to be served!"
   }
 }
+
+function currentLine(queue){
+  var lineup = []
+  if (queue.length > 0 ){
+    for (var i = 0 ; i <= queue.length - 1 ' i++'){
+      lineup.push(`${i+1}. ${queue[i]}`)
+    }
+
+    return "The line is currently: " + lineup
+  } else {
+    return "The line is currently empty."
+  }
+}
