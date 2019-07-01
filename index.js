300d92014f46b221b7e910dd18f24fce099d30cd
Donelle Reed <donellereed@gmail.com>
2019-07-01 23:14:54 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,3 +2,11 @@ function takeANumber(queue, customer){
   queue.push(customer);
   return `Welcome, ${customer}. You are number ${queue.length} in line.`
 }
+
+function nowServing (queue){
+  if (queue.length > 0){
+    return queue.slice(0,1)
+  } else {
+    return "There is nobody waiting to be served!"
+  }
+}
